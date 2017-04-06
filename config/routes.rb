745309620629583dbb1bd8ebd931a3bf577f0cca
ryt11 <rileyt_2000@hotmail.com>
2017-04-06 16:20:36 -0600

    add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
 
-  resources :videos, only: [:new, :create]
+  resources :videos, only: [:new, :create, :show]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
