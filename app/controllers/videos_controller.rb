0494ef65634a1ccadcb2f58de5ed5444e7151879
Riley Thompson <rileyt_2000@hotmail.com>
2017-04-18 18:57:22 -0600

    add peggo redirect form

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,14 @@
 class VideosController < ApplicationController
+
+  before_action except:[:new, :create] { params[:peggo_redirect][:youtube_url] = youtube_sanitize(params[:peggo_redirect][:youtube_url]) }
+
   def new
     @video = Video.new
   end
 
   def create
-    @video = current_user.videos.new(audio: params[:video][:audio])
-    
+    @video = current_user.videos.new(audio: params[:video][:audio]) #strong params were failing here, could try to fix later
+
     if @video.save
       flash[:success] = "Depicted!"
       redirect_to user_path(current_user)
@@ -14,6 +17,13 @@ class VideosController < ApplicationController
       render :new
     end
   end
+
+  def youtube_redirect
+    peggo_url = 'http://peggo.tv/dvr/' + youtube_params[:youtube_url]
+    redirect_to(peggo_url)
+  end
+
+
   #
   # private
   #
@@ -21,4 +31,12 @@ class VideosController < ApplicationController
   #  def video_params
   #   params.require(:video).permit(:audio)
   # end
+
+  def youtube_params
+    params.require(:peggo_redirect).permit(:youtube_url)
+  end
+
+  def youtube_sanitize(url)
+    url.split('=')[1]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
 
   resources :videos, only: [:new, :create, :show]
-
+  post '/youtube_redirect', to: 'videos#youtube_redirect'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  root :to => "videos#new"
 end
